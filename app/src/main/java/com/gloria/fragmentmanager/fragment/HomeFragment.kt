d61d4dedbce13b7607ee860117ae0f7d026b90c9
glucicoools <gloriaegedi@gmail.com>
2021-07-14 06:46:51 -0700

    FragmentApp

--- app/src/main/java/com/gloria/fragmentmanager/fragment/HomeFragment.kt
+++ app/src/main/java/com/gloria/fragmentmanager/fragment/HomeFragment.kt
@@ -8,21 +8,15 @@ import android.view.ViewGroup
 import com.gloria.fragmentmanager.R
 
 class HomeFragment : Fragment() {
+private lateinit var binding: FragmentHomeBinding
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
 
